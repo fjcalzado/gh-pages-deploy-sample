f2d6076dae07300da4b1cd3acc4562dea6ed31b1
fjcalzado <fjcalzadosp@gmail.com>
2017-12-18 16:54:53 +0100

    Text changed

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { SimpleComponent } from "./component";
 
-ReactDOM.render(<SimpleComponent content="This is text (modified) from parent" />, document.getElementById("root"));
+ReactDOM.render(
+  <SimpleComponent content="This is text (latest version) from parent" />,
+  document.getElementById("root")
+);
