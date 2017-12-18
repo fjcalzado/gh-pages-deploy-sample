3fefd4a2d273ecb24ddb9aa760573841f999c0b8
fjcalzado <fjcalzadosp@gmail.com>
2017-12-18 16:20:08 +0100

    Update text from parent

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { SimpleComponent } from "./component";
 
-ReactDOM.render(<SimpleComponent content="This is text from parent" />, document.getElementById("root"));
+ReactDOM.render(<SimpleComponent content="This is text (modified) from parent" />, document.getElementById("root"));
