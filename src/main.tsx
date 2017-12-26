8b6ecc9a0e0ece3edea33f44f544c735b533cf8e
fjcalzado <fjcalzadosp@gmail.com>
2017-12-26 19:03:08 +0100

    Update source version and package.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import * as ReactDOM from "react-dom";
 import { SimpleComponent } from "./component";
 
 ReactDOM.render(
-  <SimpleComponent content="This is text (latest version) from parent" />,
+  <SimpleComponent content="This is text (version 19.02) from parent" />,
   document.getElementById("root")
 );
