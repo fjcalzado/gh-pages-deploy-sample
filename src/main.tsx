07829b02cb30d82518d37ade7a381989749ddaa9
fjcalzado <fjcalzadosp@gmail.com>
2017-12-27 08:02:06 +0100

    Update source

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import * as ReactDOM from "react-dom";
 import { SimpleComponent } from "./component";
 
 ReactDOM.render(
-  <SimpleComponent content="This is version: Second Automated Deploy" />,
+  <SimpleComponent content="This is version: v.3 Automated Deploy with custom commit message" />,
   document.getElementById("root")
 );
